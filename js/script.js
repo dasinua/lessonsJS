18aeeff67712481cba798827a7701800c0bded0b
Aleksandr <dasinua@gmai.com>
2021-07-25 20:44:11 +0300

    practice 2

--- js/script.js
+++ js/script.js
@@ -9,12 +9,25 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for(let i = numberOfFilms; i > 0; i--){
+    const question1 = prompt('Один из последних просмотренных фильмов?', '');
+    if(question1.length > 0 && question1.length < 50){
+        const question2 = prompt('На сколько оцените его?', '');
+        personalMovieDB.movies[question1] = question2;
+    }else {
+        break;
+    }
+    
+    if(personalMovieDB.count < 10){
+        console.log("Просмотрено довольно мало фильмов");
+    } else if(personalMovieDB.count > 10 && personalMovieDB.count < 30){
+        console.log("Вы классический зритель");
+    } else {
+        console.log("Вы киноман");
+    }
+    
+}
 
 console.log(personalMovieDB);
